f3acc56d18f2ce6fd5611bad2a44f92122d1a53c
MickMake <github@mickmake.com>
2022-12-15 19:33:07 +1100

    v3.0.0

--- GoSungrow/src/run.sh
+++ GoSungrow/src/run.sh
@@ -34,62 +34,70 @@ export GOSUNGROW_APPKEY="$(jq --raw-output '.sungrow_appkey // empty' ${CONFIG_P
 export GOSUNGROW_DEBUG="$(jq --raw-output '.debug // empty' ${CONFIG_PATH})"
 export GOSUNGROW_TIMEOUT="$(jq --raw-output '.sungrow_timeout|tostring + "s" // empty' ${CONFIG_PATH})"
 
-# export GOSUNGROW_MQTT_HOST="$(bashio::services mqtt "host")"
-export GOSUNGROW_MQTT_HOST="core-mosquitto"
+export GOSUNGROW_MQTT_HOST="$(bashio::services mqtt "host")"
 GOSUNGROW_MQTT_HOST="$(jq --raw-output --arg default "${GOSUNGROW_MQTT_HOST}" '.mqtt_host // empty | select(. != "") // $default' ${CONFIG_PATH})"
+if [ -z "${GOSUNGROW_MQTT_HOST}" ]
+then
+	bashio::log.error "No MQTT host defined and none could be auto-detected."
+	exit -1
+fi
 
-# export GOSUNGROW_MQTT_PORT="$(jq --raw-output '.mqtt_port // empty' ${CONFIG_PATH})"
-# GOSUNGROW_MQTT_PORT="$(jq --raw-output --arg default "${GOSUNGROW_MQTT_PORT}" '.mqtt_port // empty | select(. != "") // $default' ${CONFIG_PATH})"
+export GOSUNGROW_MQTT_PORT="$(jq --raw-output '.mqtt_port // empty' ${CONFIG_PATH})"
+GOSUNGROW_MQTT_PORT="$(jq --raw-output --arg default "${GOSUNGROW_MQTT_PORT}" '.mqtt_port // empty | select(. != "") // $default' ${CONFIG_PATH})"
+if [ -z "${GOSUNGROW_MQTT_PORT}" ]
+then
+	GOSUNGROW_MQTT_PORT="1883"
+fi
 
-# export GOSUNGROW_MQTT_USER="$(bashio::services mqtt "username")"
-# GOSUNGROW_MQTT_USER="$(jq --raw-output --arg default "${GOSUNGROW_MQTT_USER}" '.mqtt_user // empty | select(. != "") // $default' ${CONFIG_PATH})"
+export GOSUNGROW_MQTT_USER="$(bashio::services mqtt "username")"
+GOSUNGROW_MQTT_USER="$(jq --raw-output --arg default "${GOSUNGROW_MQTT_USER}" '.mqtt_user // empty | select(. != "") // $default' ${CONFIG_PATH})"
 
-# export GOSUNGROW_MQTT_PASSWORD="$(bashio::services mqtt "password")"
-# GOSUNGROW_MQTT_PASSWORD="$(jq --raw-output --arg default "${GOSUNGROW_MQTT_PASSWORD}" '.mqtt_password // empty | select(. != "") // $default' ${CONFIG_PATH})"
+export GOSUNGROW_MQTT_PASSWORD="$(bashio::services mqtt "password")"
+GOSUNGROW_MQTT_PASSWORD="$(jq --raw-output --arg default "${GOSUNGROW_MQTT_PASSWORD}" '.mqtt_password // empty | select(. != "") // $default' ${CONFIG_PATH})"
 
 
 bashio::log.info "Writing GoSungrow config ..."
 /usr/local/bin/GoSungrow config write
-# checkExit
+checkExit
 
 
 bashio::log.info "Login to iSolarCloud using gateway ${GOSUNGROW_HOST} ..."
-# /usr/local/bin/GoSungrow api login
+/usr/local/bin/GoSungrow api login
 # checkExit
 
 
 bashio::log.info "Syncing data from gateway ${GOSUNGROW_HOST} ..."
-#/usr/local/bin/GoSungrow mqtt sync
+/usr/local/bin/GoSungrow mqtt sync
 # checkExit
 
 
-# bashio::log.info "GoSungrow exited without error ..."
+bashio::log.info "GoSungrow exited without error ..."
 
-if ! bashio::services.available "mqtt"
-then
-    echo "No internal MQTT service found. Please install Mosquitto broker"
-    bashio::log.error "No internal MQTT service found. Please install Mosquitto broker"
-fi
-
-  echo "host: $(bashio::services "mqtt" "host")"
-  echo "password: $(bashio::services "mqtt" "password")"
-  echo "port: $(bashio::services "mqtt" "port")"
-  echo "username: $(bashio::services "mqtt" "username")"
-
-  bashio::log.info "host: $(bashio::services "mqtt" "host")"
-  bashio::log.info "password: $(bashio::services "mqtt" "password")"
-  bashio::log.info "port: $(bashio::services "mqtt" "port")"
-  bashio::log.info "username: $(bashio::services "mqtt" "username")"
-
-echo "################################################################################"
-set > /tmp/d1
-echo "################################################################################"
-env >> /tmp/d1
-echo "################################################################################"
-ping core-mosquitto
-
-echo "SUPERVISOR_TOKEN:${SUPERVISOR_TOKEN}"
-curl -sSL -H "Authorization: Bearer $SUPERVISOR_TOKEN" http://supervisor/services/mqtt
+#if ! bashio::services.available "mqtt"
+#then
+#    echo "No internal MQTT service found. Please install Mosquitto broker"
+#    bashio::log.error "No internal MQTT service found. Please install Mosquitto broker"
+#fi
+#
+#  echo "host: $(bashio::services "mqtt" "host")"
+#  echo "password: $(bashio::services "mqtt" "password")"
+#  echo "port: $(bashio::services "mqtt" "port")"
+#  echo "username: $(bashio::services "mqtt" "username")"
+#
+#  bashio::log.info "host: $(bashio::services "mqtt" "host")"
+#  bashio::log.info "password: $(bashio::services "mqtt" "password")"
+#  bashio::log.info "port: $(bashio::services "mqtt" "port")"
+#  bashio::log.info "username: $(bashio::services "mqtt" "username")"
+#
+#echo "################################################################################"
+#set > /tmp/d1
+#echo "################################################################################"
+#env >> /tmp/d1
+#echo "################################################################################"
+#ping core-mosquitto
+#
+#echo "SUPERVISOR_TOKEN:${SUPERVISOR_TOKEN}"
+#curl -sSL -H "Authorization: Bearer $SUPERVISOR_TOKEN" http://supervisor/services/mqtt
 
 sleep 99999999
  
