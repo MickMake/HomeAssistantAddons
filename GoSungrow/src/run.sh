643ca44aa6ed3e68ad7d62329db30440fa6d4218
MickMake <github@mickmake.com>
2022-12-15 15:19:58 +1100

    v3.0.0

--- GoSungrow/src/run.sh
+++ GoSungrow/src/run.sh
@@ -19,50 +19,50 @@ checkExit()
 		cat ${CONFIG_PATH}
 		/usr/local/bin/GoSungrow config read
 
-		# exit ${EXIT}
+		exit ${EXIT}
 	fi
 }
 
 
-bashio::log.info "Setting up GoSungrow config ..."
+# bashio::log.info "Setting up GoSungrow config ..."
 
-export GOSUNGROW_USER="$(jq --raw-output '.sungrow_user // empty' ${CONFIG_PATH})"
-export GOSUNGROW_PASSWORD="$(jq --raw-output '.sungrow_password // empty' ${CONFIG_PATH})"
-export GOSUNGROW_HOST="$(jq --raw-output '.sungrow_host // empty' ${CONFIG_PATH})"
-export GOSUNGROW_APPKEY="$(jq --raw-output '.sungrow_appkey // empty' ${CONFIG_PATH})"
+# export GOSUNGROW_USER="$(jq --raw-output '.sungrow_user // empty' ${CONFIG_PATH})"
+# export GOSUNGROW_PASSWORD="$(jq --raw-output '.sungrow_password // empty' ${CONFIG_PATH})"
+# export GOSUNGROW_HOST="$(jq --raw-output '.sungrow_host // empty' ${CONFIG_PATH})"
+# export GOSUNGROW_APPKEY="$(jq --raw-output '.sungrow_appkey // empty' ${CONFIG_PATH})"
 
-export GOSUNGROW_DEBUG="$(jq --raw-output '.debug // empty' ${CONFIG_PATH})"
-export GOSUNGROW_TIMEOUT="$(jq --raw-output '.sungrow_timeout|tostring + "s" // empty' ${CONFIG_PATH})"
+# export GOSUNGROW_DEBUG="$(jq --raw-output '.debug // empty' ${CONFIG_PATH})"
+# export GOSUNGROW_TIMEOUT="$(jq --raw-output '.sungrow_timeout|tostring + "s" // empty' ${CONFIG_PATH})"
 
-export GOSUNGROW_MQTT_HOST="$(bashio::services mqtt "host")"
-GOSUNGROW_MQTT_HOST="$(jq --raw-output --arg default "${GOSUNGROW_MQTT_HOST}" '.mqtt_host // empty | select(. != "") // $default' ${CONFIG_PATH})"
+# export GOSUNGROW_MQTT_HOST="$(bashio::services mqtt "host")"
+# GOSUNGROW_MQTT_HOST="$(jq --raw-output --arg default "${GOSUNGROW_MQTT_HOST}" '.mqtt_host // empty | select(. != "") // $default' ${CONFIG_PATH})"
 
-export GOSUNGROW_MQTT_PORT="$(jq --raw-output '.mqtt_port // empty' ${CONFIG_PATH})"
-GOSUNGROW_MQTT_PORT="$(jq --raw-output --arg default "${GOSUNGROW_MQTT_PORT}" '.mqtt_port // empty | select(. != "") // $default' ${CONFIG_PATH})"
+# export GOSUNGROW_MQTT_PORT="$(jq --raw-output '.mqtt_port // empty' ${CONFIG_PATH})"
+# GOSUNGROW_MQTT_PORT="$(jq --raw-output --arg default "${GOSUNGROW_MQTT_PORT}" '.mqtt_port // empty | select(. != "") // $default' ${CONFIG_PATH})"
 
-export GOSUNGROW_MQTT_USER="$(bashio::services mqtt "username")"
-GOSUNGROW_MQTT_USER="$(jq --raw-output --arg default "${GOSUNGROW_MQTT_USER}" '.mqtt_user // empty | select(. != "") // $default' ${CONFIG_PATH})"
+# export GOSUNGROW_MQTT_USER="$(bashio::services mqtt "username")"
+# GOSUNGROW_MQTT_USER="$(jq --raw-output --arg default "${GOSUNGROW_MQTT_USER}" '.mqtt_user // empty | select(. != "") // $default' ${CONFIG_PATH})"
 
-export GOSUNGROW_MQTT_PASSWORD="$(bashio::services mqtt "password")"
-GOSUNGROW_MQTT_PASSWORD="$(jq --raw-output --arg default "${GOSUNGROW_MQTT_PASSWORD}" '.mqtt_password // empty | select(. != "") // $default' ${CONFIG_PATH})"
-
-
-bashio::log.info "Writing GoSungrow config ..."
-/usr/local/bin/GoSungrow config write
-checkExit
+# export GOSUNGROW_MQTT_PASSWORD="$(bashio::services mqtt "password")"
+# GOSUNGROW_MQTT_PASSWORD="$(jq --raw-output --arg default "${GOSUNGROW_MQTT_PASSWORD}" '.mqtt_password // empty | select(. != "") // $default' ${CONFIG_PATH})"
 
 
-bashio::log.info "Login to iSolarCloud using gateway ${GOSUNGROW_HOST} ..."
-/usr/local/bin/GoSungrow api login
-checkExit
+# bashio::log.info "Writing GoSungrow config ..."
+# /usr/local/bin/GoSungrow config write
+# checkExit
 
 
-bashio::log.info "Syncing data from gateway ${GOSUNGROW_HOST} ..."
-/usr/local/bin/GoSungrow mqtt sync
-checkExit
+# bashio::log.info "Login to iSolarCloud using gateway ${GOSUNGROW_HOST} ..."
+# /usr/local/bin/GoSungrow api login
+# checkExit
 
 
-bashio::log.info "GoSungrow exited without error ..."
+# bashio::log.info "Syncing data from gateway ${GOSUNGROW_HOST} ..."
+# /usr/local/bin/GoSungrow mqtt sync
+# checkExit
+
+
+# bashio::log.info "GoSungrow exited without error ..."
 
 pwd
 id
