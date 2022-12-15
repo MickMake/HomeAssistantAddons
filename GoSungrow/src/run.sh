f358809fb66119744b8c8c1a92b273fadc6c85fe
MickMake <github@mickmake.com>
2022-12-15 12:21:33 +1100

    v3.0.0

--- GoSungrow/src/run.sh
+++ GoSungrow/src/run.sh
@@ -3,54 +3,61 @@ set -e
 
 export CONFIG_PATH="/data/options.json"
 
+checkExit()
+{
+	EXIT="$?"
+	if [ "${EXIT}" != "0" ]
+	then
+		bashio::log.error "GoSungrow terminated with an error. Checking on things, please include this in any issue on GitHub ..."
+		sleep 10
+		set -x
+		ls -lart /usr/local/bin/
+		uname -a
+		ifconfig
+		ls -l ${CONFIG_PATH}
+		cat ${CONFIG_PATH}
+		/usr/local/bin/GoSungrow config read
+
+		exit ${EXIT}
+	fi
+}
+
+
 bashio::log.info "Setting up GoSungrow config ..."
 
 export SUNGROW_HOST="$(bashio::config 'sungrow_host')"
-if [ "${SUNGROW_HOST}" == "null" ]; then SUNGROW_HOST=""; fi
+if [ "${SUNGROW_HOST}" != "null" ]; then SUNGROW_HOST=""; fi
 
 export SUNGROW_USER="$(bashio::config 'sungrow_user')"
-if [ "${SUNGROW_USER}" == "null" ]; then SUNGROW_USER=""; fi
+if [ "${SUNGROW_USER}" != "null" ]; then SUNGROW_USER=""; fi
 
 export SUNGROW_PASSWORD="$(bashio::config 'sungrow_password')"
-if [ "${SUNGROW_PASSWORD}" == "null" ]; then SUNGROW_PASSWORD=""; fi
+if [ "${SUNGROW_PASSWORD}" != "null" ]; then SUNGROW_PASSWORD=""; fi
 
 export SUNGROW_APPKEY="$(bashio::config 'sungrow_appkey')"
-if [ "${SUNGROW_APPKEY}" == "null" ]; then SUNGROW_APPKEY=""; fi
+if [ "${SUNGROW_APPKEY}" != "null" ]; then SUNGROW_APPKEY=""; fi
 
 export SUNGROW_DEBUG="$(bashio::config 'sungrow_debug')"
-if [ "${SUNGROW_DEBUG}" == "null" ]; then SUNGROW_DEBUG=""; fi
+if [ "${SUNGROW_DEBUG}" != "null" ]; then SUNGROW_DEBUG=""; fi
 
 export SUNGROW_TIMEOUT="$(bashio::config 'sungrow_timeout')"
-if [ "${SUNGROW_TIMEOUT}" == "null" ]; then SUNGROW_TIMEOUT=""; fi
+if [ "${SUNGROW_TIMEOUT}" != "null" ]; then SUNGROW_TIMEOUT=""; fi
 
 
-export SUNGROW_MQTT_HOST="$(bashio::config 'sungrow_mqtt_host')"
-if [ "${SUNGROW_MQTT_HOST}" == "null" ]; then SUNGROW_MQTT_HOST=""; fi
+export SUNGROW_MQTT_HOST="$(bashio::services mqtt "host")"
+SUNGROW_MQTT_HOST="$(bashio::config 'sungrow_mqtt_host' "${SUNGROW_MQTT_HOST}")"
+if [ "${SUNGROW_MQTT_HOST}" != "null" ]; then SUNGROW_MQTT_HOST=""; fi
 
 export SUNGROW_MQTT_PORT="$(bashio::config 'sungrow_mqtt_port')"
-if [ "${SUNGROW_MQTT_PORT}" == "null" ]; then SUNGROW_MQTT_PORT=""; fi
+if [ "${SUNGROW_MQTT_PORT}" != "null" ]; then SUNGROW_MQTT_PORT=""; fi
 
-export SUNGROW_MQTT_USER="$(bashio::config 'sungrow_mqtt_user')"
-if [ "${SUNGROW_MQTT_USER}" == "null" ]; then SUNGROW_MQTT_USER=""; fi
+export SUNGROW_MQTT_USER="$(bashio::services mqtt "username")"
+SUNGROW_MQTT_USER="$(bashio::config 'sungrow_mqtt_user' "${SUNGROW_MQTT_USER}")"
+if [ "${SUNGROW_MQTT_USER}" != "null" ]; then SUNGROW_MQTT_USER=""; fi
 
-export SUNGROW_MQTT_PASSWORD="$(bashio::config 'sungrow_mqtt_password')"
-if [ "${SUNGROW_MQTT_PASSWORD}" == "null" ]; then SUNGROW_MQTT_PASSWORD=""; fi
-
-
-if [ -z "${SUNGROW_MQTT_HOST}" ]
-then
-	SUNGROW_MQTT_HOST="$(bashio::services mqtt "host")"
-fi
-
-if [ -z "${SUNGROW_MQTT_USER}" ]
-then
-	SUNGROW_MQTT_USER="$(bashio::services mqtt "username")"
-fi
-
-if [ -z "${SUNGROW_MQTT_PASSWORD}" ]
-then
-	SUNGROW_MQTT_PASSWORD="$(bashio::services mqtt "password")"
-fi
+export SUNGROW_MQTT_PASSWORD="$(bashio::services mqtt "password")"
+SUNGROW_MQTT_PASSWORD="$(bashio::config 'sungrow_mqtt_password' "${SUNGROW_MQTT_PASSWORD}")"
+if [ "${SUNGROW_MQTT_PASSWORD}" != "null" ]; then SUNGROW_MQTT_PASSWORD=""; fi
 
 
 DEETS="
@@ -81,27 +88,23 @@ bashio::log.info "Writing GoSungrow config ..."
 	--mqtt-user="${SUNGROW_MQTT_USER}" \
 	--mqtt-password="${SUNGROW_MQTT_PASSWORD}" \
 	--debug="${SUNGROW_DEBUG}"
+checkExit
 
 
-# bashio::log.info "Config file now reads:"
-# /usr/local/bin/GoSungrow config read
+bashio::log.info "Config file now reads:"
+/usr/local/bin/GoSungrow config read
+checkExit
 
 
 bashio::log.info "Login to iSolarCloud using gateway ${SUNGROW_HOST} ..."
 /usr/local/bin/GoSungrow api login
+checkExit
 
 
 bashio::log.info "Syncing data from gateway ${SUNGROW_HOST} ..."
 /usr/local/bin/GoSungrow mqtt sync
+checkExit
 
 
-bashio::log.info "GoSungrow terminated. Checking on things, please include this in any issue on GitHub ..."
-sleep 10
-set -x
-ls -lart /usr/local/bin/
-uname -a
-ifconfig
-ls -l ${CONFIG_PATH}
-cat ${CONFIG_PATH}
-/usr/local/bin/GoSungrow config read
+bashio::log.info "GoSungrow exited without error ..."
 
