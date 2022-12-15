26172b06ef588d12e70e6789f0b626190e7b69d4
MickMake <github@mickmake.com>
2022-12-15 11:12:39 +1100

    v3.0.0

--- GoSungrow/src/setup.sh
+++ GoSungrow/src/setup.sh
@@ -15,31 +15,7 @@ checkExit()
 apk update; checkExit
 apk add --no-cache --virtual gosungrow.persist jq wget curl; checkExit
 
-cd /usr/local/bin
-ARCH="$(uname -m)"
-case "${ARCH}" in
-	'amd64'|'x86_64')
-		ARCH="amd64"
-		;;
+chmod a+x /usr/local/bin/getRelease.sh /usr/local/bin/run.sh /usr/local/bin/setup.sh
 
-	'aarch64'|'armhf'|'armv7'|'arm64')
-		ARCH="arm64"
-		;;
-
-	*)
-		bashio::log.error "Architecture not supported"
-		exit 1
-esac
-
-curl -s https://api.github.com/repos/MickMake/GoSungrow/releases/latest | grep "browser_download_url.*$(uname -m).deb" \
-| head -1 \
-| cut -d : -f 2,3 \
-| tr -d \" \
-| wget --show-progress -qi - \
-|| echo "-> Could not download the latest version of '${REPO}' for your architecture." # if you're polite
-
-
-wget -O- https://github.com/MickMake/GoSungrow/releases/download/v${GOSUNGROW_VERSION}/GoSungrow-linux_${ARCH}.tar.gz | tar zxvf - GoSungrow
-
-chmod a+x GoSungrow
+/usr/local/bin/getRelease.sh
 
