d4ca9380440ebb0f4c6dc2d79dad937d1851de91
MickMake <github@mickmake.com>
2022-12-15 11:49:28 +1100

    v3.0.0

--- GoSungrow/src/run.sh
+++ GoSungrow/src/run.sh
@@ -6,16 +6,36 @@ CONFIG_PATH="/data/config.json"
 bashio::log.info "Setting up GoSungrow config ..."
 
 export SUNGROW_HOST=$(bashio::config 'sungrow_host' '')
+if [ "${SUNGROW_HOST}" == "null" ]; then SUNGROW_HOST=""; fi
+
 export SUNGROW_USER=$(bashio::config 'sungrow_user' '')
+if [ "${SUNGROW_USER}" == "null" ]; then SUNGROW_USER=""; fi
+
 export SUNGROW_PASSWORD=$(bashio::config 'sungrow_password' '')
+if [ "${SUNGROW_PASSWORD}" == "null" ]; then SUNGROW_PASSWORD=""; fi
+
 export SUNGROW_APPKEY=$(bashio::config 'sungrow_appkey' '')
+if [ "${SUNGROW_APPKEY}" == "null" ]; then SUNGROW_APPKEY=""; fi
+
 export SUNGROW_DEBUG=$(bashio::config 'sungrow_debug' '')
+if [ "${SUNGROW_DEBUG}" == "null" ]; then SUNGROW_DEBUG=""; fi
+
 export SUNGROW_TIMEOUT=$(bashio::config 'sungrow_timeout' '')
+if [ "${SUNGROW_TIMEOUT}" == "null" ]; then SUNGROW_TIMEOUT=""; fi
+
 
 export SUNGROW_MQTT_HOST=$(bashio::config 'sungrow_mqtt_host' '')
+if [ "${SUNGROW_MQTT_HOST}" == "null" ]; then SUNGROW_MQTT_HOST=""; fi
+
 export SUNGROW_MQTT_PORT=$(bashio::config 'sungrow_mqtt_port' '')
+if [ "${SUNGROW_MQTT_PORT}" == "null" ]; then SUNGROW_MQTT_PORT=""; fi
+
 export SUNGROW_MQTT_USER=$(bashio::config 'sungrow_mqtt_user' '')
+if [ "${SUNGROW_MQTT_USER}" == "null" ]; then SUNGROW_MQTT_USER=""; fi
+
 export SUNGROW_MQTT_PASSWORD=$(bashio::config 'sungrow_mqtt_password' '')
+if [ "${SUNGROW_MQTT_PASSWORD}" == "null" ]; then SUNGROW_MQTT_PASSWORD=""; fi
+
 
 if [ -z "${SUNGROW_MQTT_HOST}" ]
 then
@@ -33,19 +53,20 @@ then
 fi
 
 
-#DEETS="
-#	SUNGROW_HOST = \"${SUNGROW_HOST}\"
-#	SUNGROW_USER = \"${SUNGROW_USER}\"
-#	SUNGROW_PASSWORD = \"${SUNGROW_PASSWORD}\"
-#	SUNGROW_APPKEY = \"${SUNGROW_APPKEY}\"
-#	SUNGROW_DEBUG = \"${SUNGROW_DEBUG}\"
-#	SUNGROW_TIMEOUT = \"${SUNGROW_TIMEOUT}\"
-#
-#	SUNGROW_MQTT_HOST = \"${SUNGROW_MQTT_HOST}\"
-#	SUNGROW_MQTT_PORT = \"${SUNGROW_MQTT_PORT}\"
-#	SUNGROW_MQTT_USER = \"${SUNGROW_MQTT_USER}\"
-#	SUNGROW_MQTT_PASSWORD = \"${SUNGROW_MQTT_PASSWORD}\"
-#"
+DEETS="
+	SUNGROW_HOST = \"${SUNGROW_HOST}\"
+	SUNGROW_USER = \"${SUNGROW_USER}\"
+	SUNGROW_PASSWORD = \"${SUNGROW_PASSWORD}\"
+	SUNGROW_APPKEY = \"${SUNGROW_APPKEY}\"
+	SUNGROW_DEBUG = \"${SUNGROW_DEBUG}\"
+	SUNGROW_TIMEOUT = \"${SUNGROW_TIMEOUT}\"
+
+	SUNGROW_MQTT_HOST = \"${SUNGROW_MQTT_HOST}\"
+	SUNGROW_MQTT_PORT = \"${SUNGROW_MQTT_PORT}\"
+	SUNGROW_MQTT_USER = \"${SUNGROW_MQTT_USER}\"
+	SUNGROW_MQTT_PASSWORD = \"${SUNGROW_MQTT_PASSWORD}\"
+"
+bashio::log.info "DEETS: $DEETS"
 
 
 bashio::log.info "Writing GoSungrow config ..."
